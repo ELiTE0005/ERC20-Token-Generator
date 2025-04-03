abd2f1edb0bc98a590d84d3950b6c017a259c64d
RAJBIR BISWAS <biswajitagt456@gmail.com>
2025-04-04 04:44:15 +0530

    Add files via upload

---
+++ ERC20.sol
@@ -0,0 +1,12 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TOKEN is ERC20 {
+    constructor() ERC20("dStake", "dStake") {
+        _mint(msg.sender, 10000000000000000000000000);
+    }
+}
+
